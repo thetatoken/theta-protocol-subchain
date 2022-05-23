68e47690d9c168b98c116ad8086f904fe7ceda93
Jieyi Long (THETA Network) <jieyi@sliver.tv>
2022-05-23 11:46:40 -0700

    MapChainID implementation for subchains

--- common/utils.go
+++ common/utils.go
@@ -2,10 +2,17 @@ package common
 
 import (
 	"math/big"
+
+	"github.com/thetatoken/theta/ledger/types"
 )
 
 const NumMainchainBlocksPerDynasty int64 = 100
 
 func CalculateDynasty(mainchainHeight *big.Int) *big.Int {
 	return new(big.Int).Div(mainchainHeight, big.NewInt(NumMainchainBlocksPerDynasty))
+}
+
+func MapChainID(chainIDStr string) *big.Int {
+	mainChainBlockHeight := uint64(1000000000) // doesn't really matter for subchains, just set it to a sufficiently large number
+	return types.MapChainID(chainIDStr, mainChainBlockHeight)
 }
