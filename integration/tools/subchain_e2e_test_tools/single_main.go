8ed02138fbac3f125df61849b671993e6a9a0305
litarne <705290832@qq.com>
2022-08-13 10:06:18 +0800

    rename dir subchain_e2e_test_tools

--- integration/tools/subchain_e2e_test_tools/single_main.go
+++ integration/tools/subchain_e2e_test_tools/single_main.go
@@ -52,13 +52,13 @@ func main() {
 	subchainTfuelBurn(big.NewInt(10))
 
 	//TNT20
-	mainchainTNT20Lock(big.NewInt(100)) // last address printed in console is subchain voucher contract address
+	mainchainTNT20Lock(big.NewInt(100)) //mainchainTNT20Lock(tokenAmount) last address printed in console is subchain voucher contract address
 	subchainTNT20Lock(big.NewInt(10))   //last address printed in console is mainchain voucher contract address
 	mainchainTNT20Burn(big.NewInt(10))
 	subchainTNT20Burn(big.NewInt(10))
 
 	//TNT721
-	mainchainTNT721Lock(big.NewInt(12)) //last address printed in console is subchain voucher contract address
+	mainchainTNT721Lock(big.NewInt(12)) //mainchainTNT721Lock(tokenid) last address printed in console is subchain voucher contract address
 	subchainTNT721Burn(big.NewInt(12))
 	subchainTNT721Lock(big.NewInt(10)) //last address printed in console is mainchain voucher contract address
 	mainchainTNT721Burn(big.NewInt(10))
