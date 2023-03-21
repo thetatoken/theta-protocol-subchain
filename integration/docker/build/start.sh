009775e2ea24dca37a4be943973b36a64b17a58f
Jieyi Long (THETA Network) <jieyi@sliver.tv>
2023-03-21 16:01:57 +0800

    Fixes for docker scripts

--- integration/docker/build/start.sh
+++ integration/docker/build/start.sh
@@ -7,15 +7,8 @@ set -x
 
 GOBIN=/usr/local/go/bin/go
 
-$GOBIN build -o ./build/linux/theta ./cmd/theta
-$GOBIN build -o ./build/linux/thetacli ./cmd/thetacli
-$GOBIN build -o ./build/linux/dump_storeview ./integration/tools/dump_storeview
-$GOBIN build -o ./build/linux/encrypt_sk ./integration/tools/encrypt_sk
-$GOBIN build -o ./build/linux/generate_genesis ./integration/tools/generate_genesis
-$GOBIN build -o ./build/linux/hex_obj_parser ./integration/tools/hex_obj_parser
-$GOBIN build -o ./build/linux/inspect_data ./integration/tools/inspect_data
-$GOBIN build -o ./build/linux/query_db ./integration/tools/query_db
-$GOBIN build -o ./build/linux/sign_hex_msg ./integration/tools/sign_hex_msg
+$GOBIN build -buildvcs=false -o ./build/linux/thetasubchain ./cmd/thetasubchain
+$GOBIN build -buildvcs=false -o ./build/linux/thetasubcli ./cmd/thetasubcli
 
 set +x 
 
